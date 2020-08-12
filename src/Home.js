1ee01c3768186106ddb4dc06a70f38b9f50d6e01
CMSTie <cmstie@gmail.com>
2020-08-12 14:33:14 -0400

    map working hard-coded

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home = props => {
         <div className='home'>
             <h1>Welcome to NEO-Tracker</h1>
             <img className='home-image' src='https://images.indianexpress.com/2019/10/asteroid_getty-1200-1.jpg' alt='Asteroid Heading Towards Earth' />
-            <h4>{'The place to track potentially hazardous Near Earth Objects'}</h4>
+            {/* <h4>{'The place to track potentially hazardous Near Earth Objects'}</h4> */}
         </div>
     )
 }
